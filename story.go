14b4371e940232a9cb6cffa763a528aa70e55ea4
pwinning1991 <ftcpjw@yahoo.com>
2019-10-14 22:29:53 -0400

    added template to type handler

--- story.go
+++ story.go
@@ -81,13 +81,17 @@ var defaultHandlerTmpl = `
   </body>
 </html>`
 
-func NewHandler(s Story) http.Handler {
-	return handler{}
+func NewHandler(s Story, t *template.Template) http.Handler {
+	if t == nil {
+		t = tpl
+	}
+	return handler{s, t}
 
 }
 
 type handler struct {
 	s Story
+	t *template.Template
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
